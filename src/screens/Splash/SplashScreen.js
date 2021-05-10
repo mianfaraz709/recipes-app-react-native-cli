0ffef1e285ccd348caa94ef0a3505cb4842dd2a0
Saadalvi92 <53086047+Saadalvi92@users.noreply.github.com>
2021-05-10 14:30:00 +0500

    Initial commit

---
+++ src/screens/Splash/SplashScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  View, 
+  Image,
+} from 'react-native';
+import styles from './styles';
+
+export default class SplashScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.photo} source={require('../../../assets/icons/cookie100.png')} />
+      </View>
+    );
+  }
+}
